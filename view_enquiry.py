291737546529622fb32416ab6f430b6cfa362d25
Kartik0504 <kartiksetia717@gmail.com>
2024-02-23 17:22:02 +0530

    by kartik

--- view_enquiry.py
+++ view_enquiry.py
@@ -5,4 +5,10 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "data\\enquiry.csv" )
 enquiries = open(filename, "r")
 data = enquiries.readlines()
-print(data)
+datalist = []
+for line in data:
+    line = line.replace("\n"," ")
+    res = line.split(",")
+    datalist.append(res)
+print(datalist)
+enquiries.close()
