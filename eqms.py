1bfc67163deed07efac8364a79c31a8689323c49
poulamibera <poulamibera201@gmail.com>
2024-02-25 01:08:18 +0530

    all functions added

--- eqms.py
+++ eqms.py
@@ -16,37 +16,40 @@ while True:
     5. Delete enquiry
     6. Exit the program''')
     print()
-    choice=int(input("Enter your choice [1-6]: "))
-    if choice==1:
-        print("Option chosen: 1. Add a product")
-        add_products.adding_products()
-        print("The product has been successfully added!!")
+    try:
+        choice=int(input("Enter your choice [1-6]: "))
+        if choice==1:
+            print("Option chosen: 1. Add a product")
+            add_products.adding_products()
+            print("The product has been successfully added!!")
 
-    elif choice==2:
-        print("Option chosen: 2. Add a source")
-        add_source.adding_source()
-        print("The source has been successfully added!!")
+        elif choice==2:
+            print("Option chosen: 2. Add a source")
+            add_source.adding_source()
+            print("The source has been successfully added!!")
 
-    elif choice==3:
-        print("Option chosen: 3. Add an enquiry")
-        add_enquiry.new_enquiry()
-        print("The enquiry has been successfully added!!")
+        elif choice==3:
+            print("Option chosen: 3. Add an enquiry")
+            add_enquiry.new_enquiry()
+            print("The enquiry has been successfully added!!")
 
-    elif choice==4:
-        print("Option chosen: 4. View enquiry")
-        print("Enquiry Details:")
-        view_enquiry.enquiry_listing()
+        elif choice==4:
+            print("Option chosen: 4. View enquiry")
+            print("Enquiry Details:")
+            view_enquiry.enquiry_listing()
 
-    elif choice==5:
-        print("Option chosen: 5. Delete enquiry")
-        delete_enquiry.deleting_enquiry()
-        print("Successfully deleted the enquiry!!")
+        elif choice==5:
+            print("Option chosen: 5. Delete enquiry")
+            delete_enquiry.deleting_enquiry()
+            print("Successfully deleted the enquiry!!")
 
-    elif choice==6:
-        print("Option chosen: 6. Exit")
-        print("Exiting the Menu....")
-        break
-    else:
-        print("Invalid input! Try again!!")
+        elif choice==6:
+            print("Option chosen: 6. Exit")
+            print("Exiting the Menu....")
+            break
+        else:
+            print("Invalid input! Try again!!")
+    except ValueError:
+            print("Wrong input. Please enter a number.")
 
 print("Thank you for choosing us, please visit again 😄")
